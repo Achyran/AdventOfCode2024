bac4e4d5b28364f7b38c9d629574c9f52ad0af72
Achyran <tobias.breitkopf@googlemail.com>
2025-01-01 22:58:28 +0100

    Implemented IDay on day 7

--- AdventOfCode2024/Day7.cs
+++ AdventOfCode2024/Day7.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode2024
 {
-    internal class Day7
+    internal class Day7 : IDay
     {
+        public string input => throw new NotImplementedException();
+
+        public string example => throw new NotImplementedException();
+
+        public string Run(bool UseInput = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Run2(bool UseInput = true)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
