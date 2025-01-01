0863debddfd0d5f0de89c2e0e350f2226d7b17fc
Achyran <tobias.breitkopf@googlemail.com>
2025-01-01 22:54:51 +0100

    First Commit

---
+++ AdventOfCode2024/IDay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024
+{
+    public interface IDay
+    {
+        public string input { get; }
+        public string example { get; }
+        public string Run(bool UseInput = true);
+        public string Run2(bool UseInput = true);
+    }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDay d = new Day6();
+
+            Console.WriteLine(d.Run(false));
+        }
+    }
+}
